9313844114e4ad43e45d8b57e1cbb9492e040260
yunhanwei <yunhanw@umich.edu>
2015-09-02 17:43:12 -0400

    modified db tables with diff foreign key constraints

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -6,28 +6,32 @@ CREATE TABLE users (
 );
 
 CREATE TABLE data_master (
+	master_id int(10) unsigned NOT NULL AUTO_INCREMENT,
 	id int(10) unsigned NOT NULL,
 	time_entered DATETIME,
 	time DATETIME,
 	datatype int(2) NOT NULL,
+	PRIMARY KEY(master_id)
 	FOREIGN KEY(id) REFERENCES users(id)
 );
 
 CREATE TABLE BG (
-	id int(10) unsigned NOT NULL,
+	data_id int(10) unsigned NOT NULL,
 	time_entered DATETIME NOT NULL,
 	value float(8,2) NOT NULL,
 	note varchar(255) COLLATE utf8_unicode_ci,
-	FOREIGN KEY(id) REFERENCES users(id)
+	PRIMARY KEY(data_id),
+	FOREIGN KEY(data_id) REFERENCES data_master(master_id)
 );
 
 CREATE TABLE insulin (
-	id int(10) unsigned NOT NULL,
+	data_id int(10) unsigned NOT NULL,
 	time_entered DATETIME NOT NULL,
 	carbDose float(8,2),
 	correctionDose float(8,2),
 	hourlyBasalDose float(8,2),
 	type varchar(255) NOT NULL,
 	note varchar(255),
-	FOREIGN KEY(id) REFERENCES users(id)
+	PRIMARY KEY(data_id)
+	FOREIGN KEY(data_id) REFERENCES data_master(master_id)
 );
